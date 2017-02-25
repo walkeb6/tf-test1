f1f24c89bcf273d3fccc9b5a13abba69b80dc884
Benjamin Walker <ben@walkeb6.local>
2017-02-25 16:02:46 -0500

    added some stuff

--- test1.py
+++ test1.py
@@ -1,4 +1,70 @@
 import tensorflow as tf
 import numpy as np
 
-print 'Hello World'
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# input and target output
+x = tf.placeholder(tf.float32,shape=[None,784])
+y_ = tf.placeholder(tf.float32,shape=[None,10])
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+
+def convpool_layer2d(x,widthx,widthy,features_in,features_out):
+    W_conv = weight_variable([widthx,widthy,features_in,features_out])
+    b_conv = bias_variable([features_out])
+    h_conv = tf.nn.relu(conv2d(x,W_conv)+b_conv)
+    h_pool = max_pool_2x2(h_conv);
+    return h_pool
+
+x_image = tf.reshape(x,[-1,28,28,1]);
+# two convolutional layers
+# first layer
+h1 = convpool_layer2d(x_image,5,5,1,32)
+# second layer
+h2 = convpool_layer2d(h1,5,5,32,64)
+
+W_fc1 = weight_variable([7*7*64,1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
+
+# apply dropout
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+
+# readout layer
+W_fc2 = weight_variable([1024,10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2)+b_fc2
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction=tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+sess=tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+
+# go
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    if i%100==0:
+        train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+        print("step %d, training accuracy %g"%(i,train_accuracy))
+    train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
+    
+print("test accuracy %g"%accuracy.eval(feed_dict={x:mnist.test.images, y_:mnist.test.labels,keep_prob:1.0}))
