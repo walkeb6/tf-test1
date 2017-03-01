cd0aac7b8c4d48793d62dd172f3e09af3751af63
Benjamin Walker <ben@walkeb6.local>
2017-02-28 22:47:23 -0500

    Updated to add two numbers together

--- test1.py
+++ test1.py
@@ -5,8 +5,20 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # input and target output
-x = tf.placeholder(tf.float32,shape=[None,784])
-y_ = tf.placeholder(tf.float32,shape=[None,10])
+x1 = tf.placeholder(tf.float32,shape=[None,784])
+x2 = tf.placeholder(tf.float32,shape=[None,784])
+x= tf.concat(1,[x1,x2])
+#print x.get_shape()
+
+y1_ = tf.placeholder(tf.float32,shape=[None,10])
+y2_ = tf.placeholder(tf.float32,shape=[None,10])
+y_ = tf.concat(1,[y1_,y2_])
+
+# get the one-hot vectors of the sum
+y1m = tf.argmax(y1_,1)
+y2m = tf.argmax(y2_,1)
+ym = tf.add(y1m,y2m)
+y_ = tf.one_hot(ym,19)
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -29,42 +41,52 @@ def convpool_layer2d(x,widthx,widthy,features_in,features_out):
     h_pool = max_pool_2x2(h_conv);
     return h_pool
 
-x_image = tf.reshape(x,[-1,28,28,1]);
+def fc_layer(x,nIn,nOut):
+    W = weight_variable([nIn,nOut])
+    b = bias_variable([nOut])
+    return tf.nn.relu(tf.matmul(x,W)+b)
+
+x_image = tf.reshape(x,[-1,28*2,28,1]);
 # two convolutional layers
 # first layer
 h1 = convpool_layer2d(x_image,5,5,1,32)
 # second layer
 h2 = convpool_layer2d(h1,5,5,32,64)
 
-W_fc1 = weight_variable([7*7*64,1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
-
-# apply dropout
+h2_flat = tf.reshape(h2,[-1,2*7*7*64])
+#h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
+h_fc1 = fc_layer(h2_flat,2*7*7*64,2048)
+# apply dropout (right now not doing this)
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
-
+# second layer
+h_fc2 = fc_layer(h_fc1,2048,100);
 # readout layer
-W_fc2 = weight_variable([1024,10])
-b_fc2 = bias_variable([10])
+n_out = 19
+W_out = weight_variable([100,n_out])
+b_out = bias_variable([n_out])
 
-y_conv = tf.matmul(h_fc1_drop, W_fc2)+b_fc2
+y_conv = tf.nn.sigmoid(tf.matmul(h_fc2, W_out)+b_out)
+
+# the classical adder
+# add the things up like a probability distribution
+
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction=tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+#correct_prediction=tf.equal(tf.argmax(y_conv,1), tf.argmax(y1_,1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+accuracy = tf.reduce_mean(tf.square(tf.subtract(y_,y_conv)))
 sess=tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
 # go
-for i in range(1000):
-    batch = mnist.train.next_batch(50)
+for i in range(10000):
+    batch1 = mnist.train.next_batch(50)
+    batch2 = mnist.train.next_batch(50)
     if i%100==0:
-        train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+        train_accuracy = accuracy.eval(feed_dict={x1:batch1[0],x2:batch2[0],y1_:batch1[1],y2_:batch2[1],keep_prob:1.0})
         print("step %d, training accuracy %g"%(i,train_accuracy))
-    train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
+    train_step.run(feed_dict={x1:batch1[0],x2:batch2[0],y1_:batch1[1],y2_:batch2[1],keep_prob:0.5})
     
-print("test accuracy %g"%accuracy.eval(feed_dict={x:mnist.test.images, y_:mnist.test.labels,keep_prob:1.0}))
+#print("test accuracy %g"%accuracy.eval(feed_dict={x1_:mnist.test.images, y1_:mnist.test.labels,keep_prob:1.0}))
